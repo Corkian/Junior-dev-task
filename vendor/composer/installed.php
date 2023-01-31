19a3e7323a50a7bc52974a42660a5baa2cfeee55
Norson <a.cosmin.panaite@gmail.com>
2023-01-31 15:44:15 +0200

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'gunarsabeltins/lamp-stack',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '392c848390f93121b59efd56108b1ecf3b6a5fe0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'gunarsabeltins/lamp-stack' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '392c848390f93121b59efd56108b1ecf3b6a5fe0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
